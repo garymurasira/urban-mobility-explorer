a789f9378301a77d0308f6c43b73e5c56aeaa171
garymurasira <g.murasira@alustudent.com>
2026-06-19 17:02:43 +0200

    Created rate_codes dimension table

--- database/schema.sql
+++ database/schema.sql
@@ -1,13 +1,20 @@
 CREATE TABLE zones (
-    location_id   INT PRIMARY KEY,
-    borough       VARCHAR(50) NOT NULL,
-    zone          VARCHAR(100) NOT NULL,
-    service_zone  VARCHAR(50),
-    centroid_lat  DECIMAL(9,6),
-    centroid_lon  DECIMAL(9,6)
+    location_id INT PRIMARY KEY,
+    borough VARCHAR(50) NOT NULL,
+    zone VARCHAR(100) NOT NULL,
+    service_zone VARCHAR(50),
+    centroid_lat DECIMAL(9,6),
+    centroid_lon DECIMAL(9,6)
 ) ENGINE=InnoDB;
 
 CREATE TABLE vendors (
-    vendor_id    INT PRIMARY KEY,
-    vendor_name  VARCHAR(50) NOT NULL
+    vendor_id INT PRIMARY KEY,
+    vendor_name VARCHAR(50) NOT NULL
+) ENGINE=InnoDB;
+
+
+
+CREATE TABLE rate_codes (
+    rate_code_id INT PRIMARY KEY,
+    description VARCHAR(50) NOT NULL
 ) ENGINE=InnoDB;

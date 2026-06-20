ba086849ab7b57f725fff854aa6edb4a7e9bccf0
garymurasira <g.murasira@alustudent.com>
2026-06-20 13:07:09 +0200

    Created TLC seed data for vendors, rate_codes and payment_types

--- database/load_data.py
+++ database/load_data.py
@@ -14,3 +14,22 @@ def get_connection():
         user=os.environ["DB_USER"],
         password=os.environ["DB_PASSWORD"],
     )
+
+
+VENDORS = [(1, "Creative Mobile Technologies"),
+           (2, "VeriFone Inc."),]
+
+RATE_CODES = [(1,  "Standard rate"),
+              (2,  "JFK"),
+              (3,  "Newark"),
+              (4,  "Nassau or Westchester"),
+              (5,  "Negotiated fare"),
+              (6,  "Group ride"),
+              (99, "Unknown"),]
+
+PAYMENT_TYPES = [(1, "Credit card"),
+                 (2, "Cash"),
+                 (3, "No charge"),
+                 (4, "Dispute"),
+                 (5, "Unknown"),
+                 (6, "Voided trip"),]
